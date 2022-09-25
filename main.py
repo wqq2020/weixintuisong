d1ab9c0559f02e64104e31f052c6f44e7e40c6e0
WWW66066 <114321234+WWW66066@users.noreply.github.com>
2022-09-25 00:18:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_weather(region):
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
+    region_url = "http://api.tianapi.com/tianqi/index".format(region, key)
     response = get(region_url, headers=headers).json()
     if response["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
